b76967a6bdbf6075d27ccdf8c04f4deb306e5cf8
Alexandre Chau <accounts@chau.moe>
2019-09-09 12:53:15 +0200

    Document HomePage class

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 /**
- * Home page class, defines actions for the home page
+ * Home page logic
  * CLIC website software
  *
  * @author  Alexandre CHAU
@@ -8,10 +8,25 @@
 import { Page } from '../page'
 import { Request, Response } from 'express'
 
+/**
+ * View model that binds the data to the template for the home page
+ * Properties of this class are directly used in template {@link home.njk}
+ */
+class HomeViewModel {
+
+}
+
+/**
+ * Home page class, defines actions for the home page
+ */
 class HomePage extends Page {
+    // template path is relative to src/ as defined in web.ts
+    template = "pages/home/home.njk"
+    viewModel = new HomeViewModel()
+
     /** @inheritdoc */
     render(req: Request, res: Response) {
-        res.render("pages/home/home.njk")
+        res.render(this.template, this.viewModel)
     }
 }
 
