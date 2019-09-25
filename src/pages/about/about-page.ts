af7e6d41d51025bb5b1c214051791ed6fd77eb7d
Alexandre Chau <accounts@chau.moe>
2019-09-25 15:42:55 +0200

    Fix news width, add about page stubs

---
+++ src/pages/about/about-page.ts
@@ -0,0 +1,38 @@
+/**
+ * About page logic
+ * CLIC website software
+ *
+ * @author Alexandre CHAU
+ */
+import { Page } from '../page'
+import { Request, Response } from 'express'
+
+/**
+ * View model that binds the data to the template for the about page
+ * Properties of this class are directly used in template {@link
+ * about-page-view.njk}
+ */
+class AboutPageViewModel {
+
+}
+
+/**
+ * About page class, defines actions for the about page
+ */
+class AboutPage extends Page {
+    viewModel: AboutPageViewModel = new AboutPageViewModel()
+
+    /** @inheritdoc */
+    render = (req: Request, res: Response) => {
+        // template path is relative to src/ as defined in web.ts
+        const template = "pages/about/about-page-view.njk"
+
+        res.render(template, this.viewModel)
+    }
+}
+
+/**
+ * Export a single instance of the about page
+ */
+const aboutPage = new AboutPage()
+export { aboutPage }
