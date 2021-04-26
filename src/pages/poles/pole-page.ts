ccaf66c66d882555a74e5f15cab4233e0e32abff
sev <hugo.elguedj@epfl.ch>
2021-04-26 18:19:18 +0200

    new branch

---
+++ src/pages/poles/pole-page.ts
@@ -0,0 +1,29 @@
+import { Page } from '../page'
+import { Request, Response } from 'express'
+import { Pole } from './pole'
+
+class PolePageViewModel {
+    pole: Pole
+
+    constructor(pole: Pole) {
+        this.pole = pole
+    }
+}
+
+class PolePage extends Page {
+    viewModel:  PolePageViewModel
+
+    constructor(pole: Pole) {
+        super()
+        this.viewModel = new PolePageViewModel(pole)
+    }
+
+    /** @inheritDoc */
+    render = (req: Request, res: Response) => {
+        const template = "pages/poles/pole-page-view.njk"
+
+        res.render(template, this.viewModel)
+    }
+}
+
+export { PolePage }
